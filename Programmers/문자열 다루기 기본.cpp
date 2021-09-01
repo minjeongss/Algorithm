9dc6615406d18d7c2c795c29130faab3dae1b041
minjeong <kkminjeong@naver.com>
2021-09-01 22:07:10 +0900

    Level 1

---
+++ Programmers/문자열 다루기 기본.cpp
@@ -0,0 +1,46 @@
+﻿//Problem
+//Complete a function, solution that checks if the length of the string s is 4 or 6 and consists of numbers only.
+//For example, if s is a234, return False, and if 1234, return True.
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    if (s.size() == 4 || s.size() == 6) {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] < 48 || s[i]>57) {
+                answer = false;
+                return answer;
+            }
+        }
+        answer = true;
+        return answer;
+    }
+    else {
+        answer = false;
+        return answer;
+    }
+    return answer;
+}
+
+int main() {
+    string number = "1234";
+    solution(number);
+    string mix = "a123";
+    solution(mix);
+
+}
+
+/*
+string에서도 sort가 직접적으로 사용이 가능하다.
+
+** 내림차순 **
+- sort(s.rbegin(),s.rend());
+- sort(s.begin(),s.end(),greater<char>());
+
+** 오름차순 **
+- sort(s.begin(),s.end());
+*/
