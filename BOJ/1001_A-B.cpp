e68638c717ee9412b460b5b4d6f951a400ee3c59
minjeong <kkminjeong@naver.com>
2021-09-01 17:16:41 +0900

    브론즈 5

---
+++ BOJ/1001_A-B.cpp
@@ -0,0 +1,15 @@
+﻿/*
+문제 주소: https://www.acmicpc.net/problem/1001
+-문제
+A,B 입력
+A-B 출력
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a - b;
+}
