3b703880fee75eea6a7976a349d25f18a2e0896e
minjeong <kkminjeong@naver.com>
2021-09-01 21:50:40 +0900

    브론즈 5

---
+++ BOJ/11654_아스키코드.cpp
@@ -0,0 +1,14 @@
+﻿/*
+문제 주소: https://www.acmicpc.net/problem/11654
+-문제
+알파벳 소문자,대문자,숫자 ->아스키 코드값 출력
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	char str;
+	cin >> str;
+	cout << (int)str;
+}
