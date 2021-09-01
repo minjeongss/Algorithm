b9792b1daf65ecd474020a829c8071df0c9fd5bf
minjeong <kkminjeong@naver.com>
2021-09-01 22:07:00 +0900

    Level 1

---
+++ Programmers/문자열 내림차순으로 배치하기.cpp
@@ -0,0 +1,31 @@
+﻿//Problem
+//Complete a function, solution that returns a new string by sorting the characters appearing in the string s in order from large to small.
+//S consists only of uppercase and lowercase letters, and uppercase letters are considered to be smaller than lowercase letters.
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+string solution(string s) {
+    sort(s.rbegin(), s.rend());
+    cout << s;
+    return s;
+}
+
+int main() {
+    string answer = "Zbcdefg";
+    solution(answer);
+}
+
+/*
+string에서도 sort가 직접적으로 사용이 가능하다.
+
+** 내림차순 **
+- sort(s.rbegin(),s.rend());
+- sort(s.begin(),s.end(),greater<char>());
+
+** 오름차순 **
+- sort(s.begin(),s.end());
+*/
