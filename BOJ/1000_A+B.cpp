b7eeceb0ebcdc2c93ff3baee0be557192632b29c
minjeong <dotoriido@gmail.com>
2021-09-01 17:03:18 +0900

    브론즈 5

---
+++ BOJ/1000_A+B.cpp
@@ -0,0 +1,15 @@
+﻿/*
+문제 주소: https://www.acmicpc.net/problem/1000
+-문제
+A,B 입력받기
+A+B 출력
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+	cin >> a >> b;
+	cout << a + b;
+}
