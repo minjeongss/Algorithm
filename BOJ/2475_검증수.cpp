283b1267916985b097bd65f5c689328a13ece26b
minjeong <kkminjeong@naver.com>
2021-09-04 13:32:35 +0900

    브론즈 5

---
+++ BOJ/2475_검증수.cpp
@@ -0,0 +1,21 @@
+﻿/*
+문제 주소: https://www.acmicpc.net/problem/2475
+-문제
+5자리 수가 주어질 때, 각 자리값들을 제곱하여 더하고 
+10으로 나눈 나머지 구하기
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num[5];
+	int sum = 0;
+	for (int i = 0; i < 5; i++) {
+		cin >> num[i];
+		num[i] = num[i] * num[i];
+		sum += num[i];
+	}
+	int total = sum % 10;
+	cout << total;
+}
