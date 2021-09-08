c47382393ca108f2080de82ce7e136f56033a760
minjeong <kkminjeong@naver.com>
2021-09-08 22:41:25 +0900

    브론즈 3

---
+++ BOJ/2441_별찍기-4.cpp
@@ -0,0 +1,31 @@
+﻿/*
+문제 주소: https://www.acmicpc.net/problem/2441
+-문제
+2440과 동일
++ 오른쪽 정렬
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <i; j++) {
+			cout << " ";
+		}
+		for (int k = N; k >= i; k--) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
+
+/*
+5 0
+4 1
+3 2
+2 3
+1 4
+*/
