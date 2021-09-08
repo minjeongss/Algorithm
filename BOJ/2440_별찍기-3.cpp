440ab1814417620f20dbcd2897bdd7231cd0b572
minjeong <kkminjeong@naver.com>
2021-09-08 22:41:19 +0900

    브론즈 3

---
+++ BOJ/2440_별찍기-3.cpp
@@ -0,0 +1,21 @@
+﻿/*
+문제 주소: https://www.acmicpc.net/problem/2440
+-문제
+1줄 별 N개
+2줄 별 N-1개
+N줄 별 1개
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		for (int k = N; k >= i; k--) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
